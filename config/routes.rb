53fb9394e1b58220983ad10b8337400b3997fbe7
andychen <andychen@andychens-MacBook-Pro.local>
2016-09-23 02:00:10 -0400

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
 
+  get 'admins/new'
+
 #  get 'members/new'
 resources :members
  
-root 'welcome#index'
-get  '/signup',  to: 'members#new'
-post '/signup',  to: 'members#create'
+ root 'welcome#index'
+ get  '/signup',  to: 'members#new'
+ post '/signup',  to: 'members#create'
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ get '/signup_admin', to: 'admins#new'
+ post '/signup', to: 'admins#create'
+ resources :admins
+
+
 end
