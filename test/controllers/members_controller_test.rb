e532f39cfad94051eac15f9dd9beca12e4a4e01c
Andy Chen <ychen75@ncsu.edu>
2016-10-04 15:37:39 -0400

    Update members_controller_test.rb

--- test/controllers/members_controller_test.rb
+++ test/controllers/members_controller_test.rb
@@ -4,5 +4,22 @@ class MembersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
     get signup_path    assert_response :success
   end
-
+  
+  def setup
+    @member = members(:Andy)
+    @other_member = members(:James)
+  end
+  
+  test "should redirect index when not logged in" do
+    get members_path
+    assert_redirected_to login_url
+  end
+  
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(other_member)
+    assert_no_different 'Member.count' do
+      delete member_path(@member)
+    end
+    assert_redirected_to root_url
+  end
 end
