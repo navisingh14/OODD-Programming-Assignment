89d5a3b6ef1dd6b98eeb42f5a6989aaa04233629
Andy Chen <ychen75@ncsu.edu>
2016-10-09 14:56:20 -0400

    Update room.rb

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,6 @@
 class Room < ApplicationRecord
-  has_many :member
+  has_and_belongs_to_many :members
+  has_and_belongs_to_many :roomhistories
   validates :number, presence: true, length: {maximum: 50}, uniqueness: {case_sensitive: false}
 
  end
